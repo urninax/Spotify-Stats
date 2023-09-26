c14e6ad64e4ee8c911043b91b8c4f40cc6014383
Maksym Shylo <mrfreezediables@gmail.com>
2023-09-26 05:51:40 +0200

    Handle file upload size exceeding

--- src/main/java/me/urninax/spotifystats/references/internal/controllers/StorageControllerAdvice.java
+++ src/main/java/me/urninax/spotifystats/references/internal/controllers/StorageControllerAdvice.java
@@ -2,18 +2,23 @@ package me.urninax.spotifystats.references.internal.controllers;
 
 import me.urninax.spotifystats.references.internal.uploading.exceptions.StorageException;
 import me.urninax.spotifystats.references.internal.uploading.responses.UploadErrorResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
+import javax.naming.SizeLimitExceededException;
 import java.time.Instant;
 
 @RestControllerAdvice
 public class StorageControllerAdvice{
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxSize;
 
-    @ExceptionHandler( {StorageException.class} )
+    @ExceptionHandler({StorageException.class})
     public ResponseEntity<?> handleStorageException(StorageException exc, WebRequest request){
         UploadErrorResponse uploadErrorResponse = new UploadErrorResponse(
                 Instant.now(),
@@ -21,6 +26,16 @@ public class StorageControllerAdvice{
                 request.getDescription(false).substring(4)
         );
 
+        return new ResponseEntity<>(uploadErrorResponse, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler({MaxUploadSizeExceededException.class})
+    public ResponseEntity<?> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException exc, WebRequest request){
+        UploadErrorResponse uploadErrorResponse = new UploadErrorResponse(
+                Instant.now(),
+                String.format("%s. Maximum file size is %s", exc.getMessage(), maxSize),
+                request.getDescription(false).substring(4)
+        );
         return new ResponseEntity<>(uploadErrorResponse, HttpStatus.NOT_ACCEPTABLE);
     }
 }
