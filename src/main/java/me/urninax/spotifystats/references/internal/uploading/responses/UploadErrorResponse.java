b6333c623ae5b4f3c85421996f818ca69058c496
Maksym Shylo <mrfreezediables@gmail.com>
2023-09-26 05:32:48 +0200

    UploadErrorResponse bug fix

--- src/main/java/me/urninax/spotifystats/references/internal/uploading/responses/UploadErrorResponse.java
+++ src/main/java/me/urninax/spotifystats/references/internal/uploading/responses/UploadErrorResponse.java
@@ -1,10 +1,14 @@
 package me.urninax.spotifystats.references.internal.uploading.responses;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UploadErrorResponse{
